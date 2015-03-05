d2dfe63caa0da3a9ca1239fc76ab863f6e4ca5dc
Rod Sampera <rod@greentomato.com.ar>
2015-03-05 10:12:54 -0300

    first commit

---
+++ client/assets/js/app.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations'
+  ])
+    .config(config)
+    .run(run).controller('CurrentPath', function($scope, $location) {
+    $scope.currentPath = $location.path();
+});
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled:false,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+ 
+  function currentUrlPath(){
+    $scope.currentPath = $location.path();
+  }
+
+})();
+
+
